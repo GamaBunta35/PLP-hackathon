7a4bc517e05425cc257dc2d3ef62a30ea78d85c2
GamaBunta35 <159938216+GamaBunta35@users.noreply.github.com>
2024-04-26 21:37:34 +0300

    Update todo.dart

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,28 +1,52 @@
 // This class represents a single Todo item with an ID, task text, and completion status.
 class ToDo {
-  String id; // Unique identifier for the todo item
-  String todoText; // Text content of the todo item
-  bool isDone; // Completion status of the todo item (true if completed, false otherwise)
+  final String _id; // Unique identifier for the todo item
+  final String _todoText; // Text content of the todo item
+  bool _isDone; // Completion status of the todo item (true if completed, false otherwise)
 
-  // Constructor for the ToDo class, with required parameters for id and todoText,
-  // and an optional parameter for isDone with a default value of false.
+  /// Constructor for the ToDo class.
+  /// [id] is the unique identifier for the todo item.
+  /// [todoText] is the text content of the todo item.
+  /// [isDone] is the completion status of the todo item, with a default value of false.
   ToDo({
-    required this.id,
-    required this.todoText,
-    this.isDone = false,
-  });
+    required String id,
+    required String todoText,
+    bool isDone = false,
+  })   : _id = id,
+        _todoText = todoText,
+        _isDone = isDone;
+
+  // Getter for id
+  String get id => _id;
+
+  // Getter for todoText
+  String get todoText => _todoText;
+
+  // Getter for isDone
+  bool get isDone => _isDone;
+
+  // Setter for isDone
+  set isDone(bool value) {
+    _isDone = value;
+  }
 
   // A static method to generate a list of predefined ToDo items.
   // This is a factory method used to create a list of sample ToDo items.
-  static List<ToDo> todoList() {
-    // Return a list of ToDo items with predefined properties.
+  static List<ToDo> generateSampleTodos() {
     return [
-      ToDo(id: '1', todoText: 'Buy groceries', isDone: true), // A completed task
-      ToDo(id: '2', todoText: 'Go shopping', isDone: true), // A completed task
-      ToDo(id: '3', todoText: 'Call lecturer', isDone: true), // A completed task
-      ToDo(id: '4', todoText: 'Call classmate'), // An incomplete task
-      ToDo(id: '5', todoText: 'Call brother'), // An incomplete task
-      ToDo(id: '6', todoText: 'Call sister'), // An incomplete task
+      ToDo(id: "1", todoText: "Buy groceries", isDone: false),
+      ToDo(id: "2", todoText: "Finish coding assignment", isDone: true),
+      ToDo(id: "3", todoText: "Go for a run", isDone: false),
     ];
+  }
+}
+
+void main() {
+  // Create a sample ToDo list
+  final todos = ToDo.generateSampleTodos();
+
+  // Print the details of each ToDo item
+  for (final todo in todos) {
+    print('ID: ${todo.id}, Text: ${todo.todoText}, Done: ${todo.isDone}');
   }
 }
