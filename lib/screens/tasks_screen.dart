8da4b8cd51515b15cc84d2ff7fab07cdd0ad1d38
GamaBunta35 <159938216+GamaBunta35@users.noreply.github.com>
2024-04-26 22:22:39 +0300

    Update tasks_screen.dart

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todolist/model/todo.dart'; // Import ToDo model class
 import 'package:todolist/widgets/todo_items.dart'; // Import ToDoItem widget
+
 // Enumeration to represent different task categories
 enum TaskCategory {
   all,
@@ -16,116 +17,52 @@ class TasksScreen extends StatefulWidget {
 }
 
 class _TasksScreenState extends State<TasksScreen> {
-  List<ToDo> todosList = ToDo.todoList(); // List of all tasks
-  List<ToDo> _foundToDo = []; // List of tasks to display based on category
-  TaskCategory _selectedCategory = TaskCategory.all; // Default selected category
+  List<ToDo> todosList = ToDo.todoList();
+  List<ToDo> _foundToDo = [];
+  TaskCategory _selectedCategory = TaskCategory.all;
+  bool _isDarkMode = false; // Indicates whether dark mode is enabled
 
   @override
   void initState() {
-    _updateTasks(); // Initialize tasks based on selected category
+    _updateTasks();
     super.initState();
   }
 
-  // Method to update the displayed tasks based on selected category
   void _updateTasks() {
     setState(() {
       switch (_selectedCategory) {
         case TaskCategory.all:
-          _foundToDo = todosList; // Display all tasks
+          _foundToDo = todosList;
           break;
         case TaskCategory.completed:
-          _foundToDo = todosList.where((todo) => todo.isDone).toList(); // Display completed tasks
+          _foundToDo = todosList.where((todo) => todo.isDone).toList();
           break;
         case TaskCategory.pending:
-          _foundToDo = todosList.where((todo) => !todo.isDone).toList(); // Display pending tasks
+          _foundToDo = todosList.where((todo) => !todo.isDone).toList();
           break;
       }
     });
   }
 
-  // Method to handle task completion status change
   void _handleToDoChange(ToDo todo) {
     setState(() {
-      todo.isDone = !todo.isDone; // Toggle completion status
-      _updateTasks(); // Update displayed tasks after status change
+      todo.isDone = !todo.isDone;
+      _updateTasks();
     });
   }
 
-  // Method to handle task deletion
   void _deleteToDoItem(String id) {
     setState(() {
-      todosList.removeWhere((item) => item.id == id); // Remove task from list
-      _updateTasks(); // Update displayed tasks after deletion
+      todosList.removeWhere((item) => item.id == id);
+      _updateTasks();
     });
   }
 
-  // Method to show dialog for adding a new task
-  Future<void> _showAddTaskDialog(BuildContext context) async {
-    String newTaskText = ''; // Text entered by the user for the new task
-
-    await showDialog(
-      context: context,
-      builder: (BuildContext dialogContext) {
-        return AlertDialog(
-          title: Text('Add New Task'),
-          content: TextField(
-            autofocus: true,
-            onChanged: (value) {
-              newTaskText = value; // Update newTaskText as the user types
-            },
-            decoration: InputDecoration(hintText: 'Enter task...'),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(dialogContext); // Close the dialog
-              },
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                // Add the new task to the list and update the tasks
-                _addNewTask(newTaskText);
-                Navigator.pop(dialogContext); // Close the dialog
-              },
-              child: Text('Add'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  // Method to add a new task to the list
-  void _addNewTask(String taskText) {
-    if (taskText.isNotEmpty) {
-      // Generate a unique ID for the new task (you may use a different approach for IDs)
-      String newTaskId = UniqueKey().toString();
-
-      // Create a new ToDo object for the new task
-      ToDo newTask = ToDo(
-        id: newTaskId,
-        todoText: taskText,
-        isDone: false,
-      );
-
-      // Add the new task to the todosList
-      setState(() {
-        todosList.add(newTask);
-        _updateTasks(); // Update the displayed tasks list
-      });
-    }
-  }
-
-  // Method to set the selected task category
-  void _setSelectedCategory(TaskCategory category, BuildContext context) {
+  // Method to toggle dark mode
+  void _toggleDarkMode() {
     setState(() {
-      _selectedCategory = category; // Update selected category
-      _updateTasks(); // Update displayed tasks based on new category
+      _isDarkMode = !_isDarkMode;
     });
-
-    // Close the drawer and navigate to the tasks screen
-    Navigator.pop(context); // Close the drawer
   }
 
   @override
@@ -233,13 +170,4 @@ class _TasksScreenState extends State<TasksScreen> {
       // Floating action button to add new task
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Show the dialog to add a new task
-          _showAddTaskDialog(context);
-        },
-        tooltip: 'Add New Task',
-        child: Icon(Icons.add),
-      ),
-      backgroundColor: const Color(0xFFCECAB7),
-    );
-  }
-}
+          //
